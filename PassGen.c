66aaaa41686d91eb06e2debf4fea73eacde39e6d
rugveddhuri <rugveddhuri@gmail.com>
2015-12-01 18:17:09 +0530

    Project Start

---
+++ PassGen.c
@@ -0,0 +1,26 @@
+/**
+ * Title : Password Generator
+ * Author: Rugved Dhuri
+ * Date  : 23 Oct 2015
+ * Description: Password Generator Program Command Line
+ */
+
+#include <stdio.h>
+#include "include/clib.h"
+
+int main(int argc, char *argv[]) {
+    int length;
+    char *pass;
+
+    if(argc > 1) {
+        length = atoi(argv[1]);
+    }else {
+        printf("Enter Password Length: ");
+        scanf("%d",&length);
+    }
+
+    pass = passGen(length);
+
+    printf("Password is: %s", pass);
+    return EXIT_SUCCESS;
+}
---
+++ include/clib.h
@@ -0,0 +1,17 @@
+/*
+ * clib.h
+ *
+ *  Created on: 23-Nov-2015
+ *  Author    : Rugved Dhuri
+ */
+
+#ifndef STDLIB_H
+ #include <stdlib.h>
+#endif
+#ifndef STDIO_H
+ #include <stdio.h>
+#endif
+#ifndef INCLUDE_CLIB_H_
+ #define INCLUDE_CLIB_H_
+ char *passGen(int);
+#endif
